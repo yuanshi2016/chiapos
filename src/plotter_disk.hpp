f48e7a3de5b12b4fe89ce0200d1b75215fbd2d32
Gwkang <975500206@qq.com>
2021-06-19 01:28:23 +0800

    windows getpid err

--- src/plotter_disk.hpp
+++ src/plotter_disk.hpp
@@ -165,7 +165,11 @@ public:
         std::cout << "Final Directory is: " << final_dirname << std::endl;
         std::cout << "Using " << (int)num_threads << " threads of stripe size " << stripe_size
                   << std::endl;
-        std::cout << "Process ID is: " << ::getpid() << std::endl;
+#ifdef _WIN32
+		std::cout << "Process ID is: " << ::GetCurrentProcessId() << std::endl;
+#else
+		std::cout << "Process ID is: " << ::getpid() << std::endl;
+#endif
 
         // Cross platform way to concatenate paths, gulrak library.
         std::vector<fs::path> tmp_1_filenames = std::vector<fs::path>();
